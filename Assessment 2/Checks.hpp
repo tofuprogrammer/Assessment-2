879021af3b7bb13f46ed1ac6449deb75abc43a61
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-21 10:03:03 +0000

    Modified wording of comments on check function.

--- Assessment 2/Checks.hpp
+++ Assessment 2/Checks.hpp
@@ -3,8 +3,8 @@
 bool isAlphabetic(const std::string& input) {
     for (char c : input) {
         if (!isalpha(c)) {
-            return false;  // If any character is not alphabetic, return false
+            return false;  // If any character is not in the alphabet, return false
         }
     }
-    return true;  // All characters are alphabetic
+    return true;  // All characters in the user's input are in the alphabet
 }
