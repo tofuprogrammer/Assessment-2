47b3100fb392e043871d6483d2803d27b1e5d5f8
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-22 11:16:44 +0000

    Refactored variables to allow for game 2 to be implemented with similar variables and removed some spaces to increase consistency.

--- Assessment 2/Game.cpp
+++ Assessment 2/Game.cpp
@@ -30,69 +30,69 @@ int main() {
         }
         
         if (gameChoice == 1) {
-            std::string guess;
-            std::string answer = words[random(0, 230)];
-            maxAttempts = (answer.length() + 1);
-            std::vector<char> answerVector(answer.begin(), answer.end());
+            std::string guessWord;
+            std::string answerWord = words[random(0, 230)];
+            maxAttempts = static_cast<int>(answerWord.length() + 1);
+            std::vector<char> answerVector(answerWord.begin(), answerWord.end());
             int answerCounter[26] = {0};
-            for (char character : answer) {
+            for (char character : answerWord) {
                 answerCounter[character - 'a']++;
             }
             
             std::cout << "Welcome to Legally Distinct Wordle! Unlike original Wordle,  the length of the word you have to guess is varibale so you will be given one more attempt than the length of the word to guess it. For example, if the answers is six letters long, you'll be given seven attempts to guess it.\n";
-            std::cout << "You have " << maxAttempts << " attempts to guess the " << answer.length() << " letter word.\n";
+            std::cout << "You have " << maxAttempts << " attempts to guess the " << answerWord.length() << " letter word.\n";
             std::cout << "If a letter is cyan, like this: (" << "\033[1;36m" << "a" << "\033[0m" << "), that means the letter is correct and in the right position.\n";
             std::cout << "If a letter is yellow, like this: (" << "\033[1;33m" << "b" << "\033[0m" << "), that means the letter is in the answer but it's in the wrong position.\n";
             std::cout << "If a letter has no special colour, like this: (c), that means you've guessed the character more times than it appears in the answer (if the letter appears in your guess more than once and it shows as blue or yellow earlier) or it doesn't appear in the answer at all.\n";
-            std::cout << "Psst, for debugging purposes, the answer is " << answer << ".\n";
+            std::cout << "Psst, for debugging purposes, the answer is " << answerWord << ".\n";
             
             while (attempts < maxAttempts && !correct){
                 std::cout << "Enter a word: ";
-                std::cin >> guess;
+                std::cin >> guessWord;
                 
-                if (std::cin.fail() || !isAlphabetic(guess)) {
+                if (std::cin.fail() || !isAlphabetic(guessWord)) {
                     std::cerr << "Invalid input!\n";
                     continue;
                 }
-                if (guess.length() != answer.length()) {
+                if (guessWord.length() != answerWord.length()) {
                     std::cerr << "Your guess does not match the length of the answer!\n";
-                    std::cout << "The answer is " << answer.length() << " letters long.\n";
+                    std::cout << "The answer is " << answerWord.length() << " letters long.\n";
                     continue;
                 }
                 
                 int guessCounter[26] = {0};
                 
-                std::transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
-                std::vector<char> guessVector(guess.begin(), guess.end());
+                std::transform(guessWord.begin(), guessWord.end(), guessWord.begin(), ::tolower);
+                std::vector<char> guessVector(guessWord.begin(), guessWord.end());
                 
-                for (size_t counter = 0; counter < guess.length(); counter++) {
-                    int characterIndex = guess[counter] - 'a';
+                for (size_t counter = 0; counter < guessWord.length(); counter++) {
+                    int characterIndex = guessWord[counter] - 'a';
                     
-                    if (guess [counter] == answer [counter]) {
-                        std::cout << "\033[1;36m" << guess[counter] << "\033[0m";
+                    if (guessWord[counter] == answerWord[counter]) {
+                        std::cout << "\033[1;36m" << guessWord[counter] << "\033[0m";
                         guessCounter[characterIndex]++;
                     }
-                    else if (answer.find(guess[counter]) != std::string::npos) {
+                    else if (answerWord.find(guessWord[counter]) != std::string::npos) {
                         if (guessCounter[characterIndex] < answerCounter[characterIndex]) {
-                            std::cout << "\033[1;33m" << guess[counter] << "\033[0m";
+                            std::cout << "\033[1;33m" << guessWord[counter] << "\033[0m";
                             guessCounter[characterIndex]++;
                         }
                         else {
-                            std::cout << guess[counter];
+                            std::cout << guessWord[counter];
                         }
                     }
                     else {
-                        std::cout << guess[counter];
+                        std::cout << guessWord[counter];
                     }
                 }
                 std::cout << '\n';
                 attempts++;
-                if (guess == answer) {
+                if (guessWord == answerWord) {
                     correct = !correct;
                 }
             }
             if (attempts == maxAttempts){
-                std::cout << "You've run out of attempts! The correct word was " << answer << ".\n";
+                std::cout << "You've run out of attempts! The correct word was " << answerWord << ".\n";
             }
             if (correct) {
                 std::cout << "You've guessed correctly!";
